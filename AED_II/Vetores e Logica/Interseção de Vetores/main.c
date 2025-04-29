82863068630051b3d25ee4b37659ce864d8213e2
mariafr73 <pessoamaria391@gmail.com>
2025-04-28 22:34:50 -0300

    Adicionando o código de main.c

--- AED_II/Vetores e Logica/Interseção de Vetores/main.c
+++ AED_II/Vetores e Logica/Interseção de Vetores/main.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+    int vet1[5], vet2[5], i, j;
 
+    printf("Digite 5 numeros para o primeiro vetor:\n");
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &vet1[i]);
+    }
+
+    printf("Digite 5 numeros para o segundo vetor:\n");
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &vet2[i]);
+    }
+
+    printf("Elementos comuns entre os dois vetores:\n");
+    for (i = 0; i < 5; i++) {
+        for (j = 0; j < 5; j++) {
+            if (vet1[i] == vet2[j]) {
+                printf("%d ", vet1[i]);
+                break; // Evita duplicar a exibição do mesmo número
+            }
+        }
+    }
+
+    printf("\n");
     return 0;
 }
