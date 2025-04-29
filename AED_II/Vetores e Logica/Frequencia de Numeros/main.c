e493d0a6a90a0d6836424d2018bdf949d7acd1d2
mariafr73 <pessoamaria391@gmail.com>
2025-04-28 22:25:57 -0300

    Adicionando o ja_contado para não ficar repetindo os numeros quando for visualizar o resiltado final

--- AED_II/Vetores e Logica/Frequencia de Numeros/main.c
+++ AED_II/Vetores e Logica/Frequencia de Numeros/main.c
@@ -2,6 +2,8 @@
 
 int main(){
     int x[10], i;
+    int ja_contado[10] = {0}; // Array para marcar números já processados
+    
     printf("Digite 10 numeros inteiros: \n");
     for(i = 0; i < 10; i++){
         scanf("%d", &x[i]);
@@ -9,10 +11,15 @@ int main(){
 
     printf("Frequencia dos numeros:\n");
     for(i = 0; i < 10; i++){
+        if (ja_contado[i]) {
+            continue; // Pula números já processados
+        }
+
         int j, cont = 0;
         for(j = 0; j < 10; j++){
             if(x[i] == x[j]){
                 cont++;
+                ja_contado[j] = 1; // Marca o número como processado
             }
         }
         printf("%d: %d\n", x[i], cont);
